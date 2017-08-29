cafebaa5f99ed5148ccd117112c539c902925236
alex <regalko7@gmail.com>
2017-08-29 08:18:59 +0500

    comit

--- landing/models.py
+++ landing/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+'''это таблица в которой мы будем хранить имя и емеил'''
+class Profile(models.Model):
+    email = models.EmailField()
+    name = models.CharField(max_length=128) #имя не может быть больше чем 128 символов
--- landing/views.py
+++ landing/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
+from .forms import ProfileForm
+from django.db import models
 
 # Create your views here.
 
 def landing(request):
 
     name = 'Шерстяная сотона!'
-    current_day = "01.01.2017"
+    #current_day = models.DateField(auto_now=True) не работает хз почему
+    current_day = '01.01.2017'
+    form = ProfileForm(request.POST or None)
 
     return render(request, 'landing/landing.html', locals())
